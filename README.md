63d5af61ec7be4acf6beed554115b54dbd846503
SergeyGuns <mcheguevara2@gmail.com>
2017-04-05 13:13:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Readme
+
+```bash
+#must by installed : nodeJS, Git
+git clone https://github.com/SergeyGuns/react-timer.git
+cd react-timer
+npm i
+npm start
+```
---
+++ app/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Search from '../Search'
+const LIST = [
+  {name:'serg'},
+  {name:'julia'},
+  {name:'mark'}
+]
+
+import './App.css'
+export default class App extends React.Component {
+
+  render() { 
+
+    return (
+      <div className='App'>
+        <Search
+
+        />
+      </div> 
+    )
+
+  }
+}
+
